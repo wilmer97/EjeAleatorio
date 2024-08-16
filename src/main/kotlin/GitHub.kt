5f5852435c65a94ee155990f28e2004d5a684d46
wilmer97 <69699883+wilmer97@users.noreply.github.com>
2024-08-16 15:43:24 -0500

    Add files via upload

--- src/main/kotlin/GitHub.kt
+++ src/main/kotlin/GitHub.kt
@@ -1,5 +1,19 @@
 import kotlin.random.Random
 
-fun main(){
+fun main() {
 
+    //Lanzamiento de moneda
+
+    val resultados = mutableListOf<String>()
+    val random = java.util.Random()
+
+    for (i in 1..20) {
+        val lanzamiento = if (random.nextBoolean()) "Cara" else "Cruz"
+        resultados.add(lanzamiento)
+    }
+
+    println("Resultados de los 20 lanzamientos:")
+    resultados.forEachIndexed { index, resultado ->
+        println("Lanzamiento ${index + 1}: $resultado")
+    }
 }
